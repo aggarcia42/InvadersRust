2828f317b5b2705d2b1672d91bc299fb18557a6a
aggarcia42 <agusgarciaibero@gmail.com>
2022-10-14 21:14:08 +0200

    Invaders: Shooting logic
    Shooting logic and player logic for shooting

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -54,7 +54,10 @@ fn main() -> Result <(), Box<dyn Error>> {
     });
 
     let mut player = Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop{
+        let delta = instant.elapsed();
+        instant = Instant::now();
         let mut curr_frame = new_frame();
 
         while event::poll(Duration::default())?{
@@ -62,6 +65,11 @@ fn main() -> Result <(), Box<dyn Error>> {
                 match key_event.code {
                     KeyCode::Left => player.move_left(),
                     KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter =>{
+                        if player.shoot(){
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') =>{
                         audio.play("lose");
                         break 'gameloop;
@@ -70,6 +78,8 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
+
+        player.update(delta);
 
         player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
