3f620a4aa6df74783d6d807d247b41929b44a51c
aggarcia42 <agusgarciaibero@gmail.com>
2022-10-14 20:42:34 +0200

    Invaders: Screen render

---
+++ invaders/src/main.rs
@@ -0,0 +1,81 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use rusty_audio::Audio;
+use std::{
+    error::Error,
+    sync::mpsc::{self, Receiver},
+    time::{Duration, Instant},
+    {io, thread},
+};
+
+
+use invaders::{
+    frame::{self, new_frame, Drawable, Frame},
+//    invaders::Invaders,
+//    level::Level,
+ //   menu::Menu,
+ //   player::Player,
+    render,
+//    score::Score,
+};
+
+fn main() -> Result <(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explode", "./sounds/explode.wav");
+    audio.add("lose", "./sounds/lose.wav");
+    audio.add("move", "./sounds/move.wav");
+    audio.add("pew", "./sounds/pew.wav");
+    audio.add("startup", "./sounds/startup.wav");
+    audio.add("win", "./sounds/win.wav");
+    audio.play("startup");
+
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move ||{
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
+    'gameloop: loop{
+        let curr_frame = new_frame();
+
+        while event::poll(Duration::default())?{
+            if let Event::Key(key_event) = event::read()?{
+                match key_event.code {
+                    KeyCode::Esc | KeyCode::Char('q') =>{
+                        audio.play("lose");
+                        break 'gameloop;
+                    }
+                    _ => {}
+                }
+            }
+        }
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    drop(render_tx);
+    render_handle.join().unwrap();
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
