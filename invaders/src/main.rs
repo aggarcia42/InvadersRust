cf25852108096b4a62e0a481e855ce7873a069a5
aggarcia42 <agusgarciaibero@gmail.com>
2022-10-14 21:50:49 +0200

    invaders: basic invaders done
    Basic invaders done, maybe implement more stuff in near future.

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -85,6 +85,10 @@ fn main() -> Result <(), Box<dyn Error>> {
         {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders)
+        {
+            audio.play("explode");
+        }
 
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
         for drawable in drawables{
@@ -92,6 +96,16 @@ fn main() -> Result <(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
+
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom()
+        {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
 
     drop(render_tx);
--- invaders/src/player.rs
+++ invaders/src/player.rs
@@ -1,6 +1,6 @@
 use crate::{
     {NUM_COLS, NUM_ROWS},
-    frame::{Drawable, Frame},
+    frame::{Drawable, Frame}, invaders::{self, Invaders},
 };
 use crate::shot::Shot;
 
@@ -47,6 +47,22 @@ impl Player {
             shot.update(delta);
         }
         self.shots.retain(|shot| !shot.dead());
+    }
+
+    pub fn detect_hits(&mut self, invaders: &mut Invaders)-> bool{
+        let mut hit_something = false;
+
+        for shot in self.shots.iter_mut(){
+            if !shot.exploding{
+                if invaders.kill_invader_at(shot.x, shot.y)
+                {
+                    hit_something = true;
+                    shot.explode();
+                }
+            }
+        }
+
+        hit_something
     }
 }
 
